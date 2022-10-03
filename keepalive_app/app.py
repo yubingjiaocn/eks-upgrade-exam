7067c8f716e471fa33b2268f7e15a16e3973030a
yubingjiaocn <yubingjiaocn@gmail.com>
2022-10-03 13:23:01 +0800

    Add permission and scrape kube-proxy daemonset

--- keepalive_app/app.py
+++ keepalive_app/app.py
@@ -13,6 +13,7 @@ def getInfo():
     temp_dict["candidate"] = getCandidate()
     temp_dict["version"] = getVersion()
     temp_dict["nodes"] = getNodes()
+    temp_dict["workloads"] = getWorkloads()
     return json.dumps(temp_dict)
 
 def getCandidate():
@@ -21,7 +22,10 @@ def getCandidate():
 
 def getVersion():
     temp_dict={}
+
     versionAPI = client.VersionApi()
+
+    #Get cluster version
     version = versionAPI.get_code()
     temp_dict["minor"] = version.minor
     temp_dict["gitVersion"] = version.git_version
@@ -30,7 +34,10 @@ def getVersion():
 def getNodes():
     temp_dict={}
     temp_list=[]
+
     coreAPI = client.CoreV1Api()
+
+    # Get all node info
     client_output = coreAPI.list_node()
     json_data=client.ApiClient().sanitize_for_serialization(client_output)
 
@@ -45,6 +52,18 @@ def getNodes():
             temp_list.append(temp_dict)
 
     return temp_list
+
+def getWorkloads():
+    temp_dict={}
+
+    appAPI = client.AppsV1Api()
+
+    # Get kube-proxy daemonset
+    api_response = appAPI.read_namespaced_daemon_set("kube-proxy", "kube-system")
+    json_data=client.ApiClient().sanitize_for_serialization(api_response)
+    temp_dict["kube-proxy"] = json_data["spec"]["template"]["spec"]["containers"][0]["image"]
+
+    return temp_dict
 
 @app.route('/')
 def default():
